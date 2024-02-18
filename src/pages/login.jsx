d9405351d7639581227140ba940d1db80165addd
AdityaInnovatily <aditya.v@innovatily.com>
2024-02-18 14:35:16 +0530

    login and register skin

---
+++ src/pages/login.jsx
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import "./login.css";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+
+export const Login = (()=>{
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // Handle login logic here
+      console.log('Logging in with email:', email, 'and password:', password);
+    };
+  
+    const handleRegister = () => {
+      // Handle registration logic here
+      console.log('Registering with email:', email, 'and password:', password);
+    };
+
+    return(
+    <>
+
+    <div className='loginPage'>
+
+        <div className='loginPageContent'>
+
+        <div className='loginPageImage'>
+        <img alt = "Image Place" src = "https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww"></img>
+        </div>
+
+        <div className='loginPageForm'>
+
+            <div className='loginPageFormContent'>
+
+            <div className='loginPageFormContentHeader'>Login</div>
+          
+
+            <form className='form'>
+
+           
+          <div>
+
+            <div className='loginPageFormContentEmailContent'>
+            <div className='loginPageFormContentEmail'>
+          <input
+            type="email"
+            id="loginPageFormContentEmail"
+            placeholder='&#xf0e0;   Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+           <VisibilityOutlinedIcon style={{color:"#ffffff", fontSize: 20 }}/>
+      
+        </div>
+       
+        <p id = "loginPageFormContentEmailError"></p>
+      
+        </div>
+
+
+        <div className='loginPageFormContentPasswordContent'>
+        <div className='loginPageFormContentPassword'>
+          <input
+            type="password"
+            id="loginPageFormContentPassword"
+            placeholder='&#xf023;   Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+     
+          <VisibilityOutlinedIcon style={{color:"#828282", fontSize: 20 }}/>
+      
+        </div>
+
+        <p id = "loginPageFormContentPasswordError"></p>
+      
+        </div>
+
+        </div>
+
+
+        <div className='loginPageFormContentButtons'>
+          <button type = "button" className="loginPageFormContentLoginButton" onClick={handleLogin}>
+            Log in
+          </button>
+          <p id= "loginPageFormContentHaveNoAccountYet">Have no account yet?</p>
+          <button type = "button" className="loginPageFormContentRegisterButton" onClick={handleRegister}>
+            Register
+          </button>
+        </div>
+
+      </form>
+
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    </>
+
+    )
+});
---
+++ src/pages/register.jsx
@@ -0,0 +1,141 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import "./register.css";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+
+export const Register = (()=>{
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // Handle login logic here
+      console.log('Logging in with email:', email, 'and password:', password);
+    };
+  
+    const handleRegister = () => {
+      // Handle registration logic here
+      console.log('Registering with email:', email, 'and password:', password);
+    };
+
+    return(
+    <>
+
+    <div className='registerPage'>
+
+        <div className='registerPageContent'>
+
+        <div className='registerPageImage'>
+        <img alt = "Image Place" src = "https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww"></img>
+        </div>
+
+        <div className='registerPageForm'>
+
+            <div className='registerPageFormContent'>
+
+            <div className='registerPageFormContentHeader'>Register</div>
+          
+
+            <form className='form'>
+
+           
+          <div>
+
+          <div className='registerPageFormContentNameContent'>
+            <div className='registerPageFormContentName'>
+          <input
+            type="text"
+            id="registerPageFormContentName"
+            placeholder='&#xf007;   Name'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+           <VisibilityOutlinedIcon style={{color:"#ffffff", fontSize: 20 }}/>
+      
+        </div>
+       
+        <p id = "registerPageFormContentNameError">js;fls;fds</p>
+      
+        </div>
+
+            <div className='registerPageFormContentEmailContent'>
+            <div className='registerPageFormContentEmail'>
+          <input
+            type="email"
+            id="registerPageFormContentEmail"
+            placeholder='&#xf0e0;   Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+           <VisibilityOutlinedIcon style={{color:"#ffffff", fontSize: 20 }}/>
+      
+        </div>
+       
+        <p id = "registerPageFormContentEmailError">js;fls;fds</p>
+      
+        </div>
+
+
+        <div className='registerPageFormContentPasswordContent'>
+        <div className='registerPageFormContentPassword'>
+          <input
+            type="password"
+            id="registerPageFormContentPassword"
+            placeholder='&#xf023;   Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+     
+          <VisibilityOutlinedIcon style={{color:"#828282", fontSize: 20 }}/>
+      
+        </div>
+
+        <p id = "registerPageFormContentPasswordError">js;fls;fds</p>
+      
+        </div>
+
+        <div className='registerPageFormContentConfirmPasswordContent'>
+        <div className='registerPageFormContentConfirmPassword'>
+          <input
+            type="password"
+            id="registerPageFormContentConfirmPassword"
+            placeholder='&#xf023;   Confirm Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+     
+          <VisibilityOutlinedIcon style={{color:"#828282", fontSize: 20 }}/>
+      
+        </div>
+
+        <p id = "registerPageFormContentConfirmPasswordError">js;fls;fds</p>
+      
+        </div>
+
+        </div>
+
+
+        <div className='registerPageFormContentButtons'>
+        <button type = "button" className="registerPageFormContentRegisterButton" onClick={handleRegister}>
+            Register
+          </button>
+         
+          <p id= "registerPageFormContentHaveNoAccountYet">Have an account ?</p>
+          <button type = "button" className="registerPageFormContentLoginButton" onClick={handleLogin}>
+            Log in
+          </button>
+
+        </div>
+
+      </form>
+
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    </>
+
+    )
+});
